e725c28bda06d9ac2f3dd44a5e99edc664cb39ae
singhxabhijeet <abhiiamsingh007@gmail.com>
2024-06-04 03:20:18 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import zlib
 
 
 def main():
@@ -15,7 +16,14 @@ def main():
         os.mkdir(".git/refs")
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
-            print("Initialized git directory")
+        print("Initialized git directory")
+    elif command == "cat-file":
+        if sys.argv[2] == "-p":
+            blob_sha = sys.argv[3]
+            with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", "rb") as f:
+                raw = zlib.decompress(f.read())
+                header, content = raw.split(b"\0", maxsplit=1)
+                print(content.decode("utf-8"), end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
