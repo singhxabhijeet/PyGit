b3446fe124d0e9fd16130ab5f4d6edef92e61546
singhxabhijeet <abhiiamsingh007@gmail.com>
2024-06-05 09:46:22 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -4,6 +4,45 @@ import zlib
 import hashlib
 
 
+def create_blob_entry(path, write=True):
+    with open(path, "rb") as f:
+        data = f.read()
+    header = f"blob {len(data)}\0".encode("utf-8")
+    store = header + data
+    sha = hashlib.sha1(store).hexdigest()
+    if write:
+        os.makedirs(f".git/objects/{sha[:2]}", exist_ok=True)
+        with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
+            f.write(zlib.compress(store))
+    return sha
+
+
+def write_tree(path: str):
+    if os.path.isfile(path):
+        return create_blob_entry(path)
+    contents = sorted(
+            os.listdir(path),
+            key=lambda x: x if os.path.isfile(os.path.join(path, x)) else f"{x}/",
+    )
+    s = b""
+    for item in contents:
+        if item == ".git":
+            continue
+        full = os.path.join(path, item)
+        if os.path.isfile(full):
+            s += f"100644 {item}\0".encode()
+        else:
+            s += f"40000 {item}\0".encode()
+        sha1 = int.to_bytes(int(wrie_tree(full), base=16), length=20, byteorder-"big")
+        s += sha1
+    s = f"tree {len(s)}\0".encode()
+    sha1 = hashlib.sha1(s).hexdigest()
+    os.makedirs(f".git/objects/{sha1[:2]}", exist_ok=True)
+    with open(f".git/objects/{sha[:2]}/{sha[2:]}", "wb") as f:
+        f.write(zlib.compress(s))
+    return sha1
+
+
 def hash_object(object_type, file_name):
     with open(file_name, "rb") as file:
         file_content = file.read()
